ac722ac5df8245387c48afe97d86aecee63e294f
alifsyahpanji <alifsyahpanji@gmail.com>
2022-07-21 13:20:05 +0700

    mengupdate file

---
+++ assets/data.js
@@ -0,0 +1,20 @@
+let data = [
+    {
+        title: "Alifsyah Crypto",
+        description: "Ini adalah website market cryptocurency, tempat untuk bertransaksi dan menyimpan aset crypto. Website ini menggunakan React js agar terhubung ke database REST API Market Crypto yang terupdate.",
+        image: "assets/portfolio3.jpg",
+        link: "https://alifsyahpanji.github.io/portfolio3"
+    },
+    {
+        title: "Alifsyah Anime",
+        description: "Ini adalah website anime yang dinamis, dengan menggunakan framework React js membuat website ini terhubung langsung dengan database Rest API anime yang terupdate",
+        image: "assets/portfolio2.jpg",
+        link: "https://alifsyahpanji.github.io/portfolio2"
+    },
+    {
+        title: "Alifsyah Studio",
+        description: "Website untuk tempat usaha studio musik dengan gaya tampilan seperti blog namun terlihat menarik",
+        image: "assets/portfolio1.jpg",
+        link: "https://alifsyahpanji.github.io/portfolio1"
+    }
+];
